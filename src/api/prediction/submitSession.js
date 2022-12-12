d74d9a44570fd320839b4fc917531374f7ef5db1
a124135 <119551209+a124135@users.noreply.github.com>
2022-12-12 22:41:53 +0800

    Update submitSession.js

--- src/api/prediction/submitSession.js
+++ src/api/prediction/submitSession.js
@@ -1,11 +1,12 @@
 import request from '../../utils/request'
-export function submitSession(formData) {
-    console.log(formData)
+//提交开启预报名的场次
+export function submitSession(session) {
+    console.log(session)
     return request({
         url: '/prediction/ModifyPrediction',
         method: "post",
         data: {
-            formData
+            session
         },
     })
 }
