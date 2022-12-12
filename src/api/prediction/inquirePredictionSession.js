884655ff9337b84522237b9fb425568a8da6f840
a124135 <119551209+a124135@users.noreply.github.com>
2022-12-12 22:39:52 +0800

    Delete inquirePredictionSession.js

--- src/api/prediction/inquirePredictionSession.js
+++
@@ -1,8 +0,0 @@
-import request from '../../utils/request'
-export function inquirePredictionSession () {
-    return request({
-        url: '/prediction/PredictionSession',
-        method: "get",
-        data:'',
-    })
-}
