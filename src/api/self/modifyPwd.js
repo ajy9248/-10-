84196589a6c38f4370135286480284423ac65284
a124135 <119551209+a124135@users.noreply.github.com>
2022-12-16 19:10:16 +0800

    Update modifyPwd.js

--- src/api/self/modifyPwd.js
+++ src/api/self/modifyPwd.js
@@ -1,9 +1,9 @@
 import request from '../../utils/request'
 //进入个人信息页面提交修改好的密码
-export function modifyPwd() {
+export function modifyPwd(form) {
     return request({
         url: '/self/ModifyPwd',
         method: "post",
-        data: '',
+        data: {form},
     })
 }
