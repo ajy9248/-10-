bff442cb8bc8f81220cdd4ba50de66739981b49e
a124135 <119551209+a124135@users.noreply.github.com>
2022-12-16 18:50:28 +0800

    Update CheckLogin.js

--- src/api/CheckLogin.js
+++ src/api/CheckLogin.js
@@ -1,11 +1,11 @@
 //检查登录用户是否存在于数据库中
 import request from '../utils/request'
 export function CheckLogin (user) {
-    if(user.uid == ''){
+    if(user.username == ''){
         alert('请填写用户名');
         return;
       }
-      if(user.pwd == ''){
+      if(user.password == ''){
         alert('请填写密码');
         return;
     }
