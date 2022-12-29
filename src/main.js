84217a0715710bf274bed135cb983d4409abe3b2
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-30 00:22:14 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-undef */
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from "axios"
 import router from './routers'
 import ElementPlus from 'element-plus'
 import './assets/css/global.css'
 import 'element-plus/dist/index.css'
 import 'vue3-layer/dist/s3Layer.css';
-import "element-ui/lib/theme-chalk/index.css";
-import { read, utils } from "xlsx";
-Vue.use(ElementUI);
+import  Chart  from 'echarts';
 createApp(App).use(router).use(ElementPlus).mount('#app');
 
