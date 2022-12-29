c4fe818e6a0ab59403c05cf8536956b66946fa37
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-30 00:20:12 +0800

    Delete inquireScores.js

--- src/api/inquireScores.js
+++
@@ -1,9 +0,0 @@
-//查询所有学生成绩
-import request from '../../utils/request'
-    export function inquireScores () {
-    return request({
-        url: '/score/ScoreInfo',
-        method: "get",
-        data: '',
-    })
-}
