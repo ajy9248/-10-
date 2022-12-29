3ccd74a16d7fd1c9c96be0c6a7b8d8dc195aa65d
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-29 23:30:47 +0800

    Delete inquirePredictionByType.js

--- src/api/prediction/inquirePredictionByType.js
+++
@@ -1,9 +0,0 @@
-import request from '../../utils/request'
-//根据类型（自费、免费）查询预报名信息
-export function inquirePredictionByType (type) {
-    return request({
-        url: '/prediction/PredictionByType',
-        method: "post",
-        data:{type},
-    })
-}
