b02bf8bb0d820ee2cd1e3d3dfa2d56df201eccdf
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-30 00:20:24 +0800

    Delete scoresImport.js

--- src/api/scoresImport.js
+++
@@ -1,9 +0,0 @@
-//把导入的成绩excel表传到后端
-import request from '../../utils/request'
-export function scoresImport(scoresdata) {
-    return request({
-        url: '/score/InsertScore',
-        method: "post",
-        data: { scoresdata},
-    })
-}
