34c2ef1c1821119cd899c2fac58e0706f1621a24
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-29 23:32:07 +0800

    Add files via upload

--- src/api/self/modifyPwd.js
+++ src/api/self/modifyPwd.js
@@ -1,9 +1,12 @@
 import request from '../../utils/request'
 //进入个人信息页面提交修改好的密码
 export function modifyPwd(form) {
+    let Data = "uid="+form.username+"&old_pwd="+form.password+"&new_pwd="+form.newPassword+"&check_pwd="+form.checkPassword;
+    Data = Data.replace(/\s*/g,"");
+    console.log(Data);
     return request({
-        url: '/self/ModifyPwd',
+        url: '/user/ModifyPwd',
         method: "post",
-        data: {form},
+        data: Data,
     })
 }
