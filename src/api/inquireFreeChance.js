7f394c1797d19c4835131d0d9364561193181b32
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-29 23:33:54 +0800

    Delete inquireFreeChance.js

--- src/api/inquireFreeChance.js
+++
@@ -1,8 +0,0 @@
-import request from '../../utils/request'
-export function inquireFreeChance (user) {
-    return request({
-        url: '/login/inquireFreeChance',
-        method: "get",
-        data: {user},
-    })
-}
