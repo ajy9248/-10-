c4a851bcbe0d4debf8b3e8af0eb071c694f97a62
because-its-spring <105272148+because-its-spring@users.noreply.github.com>
2022-12-30 00:22:40 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,47 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+
+  devServer: {
+    //设置主机地址
+    host : 'localhost',
+    //设置默认端口
+    port : 8080,
+    //设置自动打开浏览器
+    open : true,
+
+    proxy: {
+      '/prediction/*': {
+        target: 'http://localhost:8084/',
+        secure: false,
+        changeOrigin: true,
+        ws: false
+      },
+      '/student/*': {
+        target: 'http://localhost:8084/',
+        secure: false,
+        changeOrigin: true,
+        ws: false
+      },
+      '/teacher/*': {
+        target: 'http://localhost:8084/',
+        secure: false,
+        changeOrigin: true,
+        ws: false
+      },
+      '/score/*': {
+        target: 'http://localhost:8084/',
+        secure: false,
+        changeOrigin: true,
+        ws: false
+      },
+      '/datacenter/*': {
+        target: 'http://localhost:8084/',
+        secure: false,
+        changeOrigin: true,
+        ws: false
+      }
+    }
+  }
 })
